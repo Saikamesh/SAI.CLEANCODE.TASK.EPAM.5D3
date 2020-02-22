0d96979773a45540fe3bac8d08aa77c58d69c998
Sai1919 <51651413+Saikamesh@users.noreply.github.com>
2020-02-22 13:13:48 +0530

    Update simpleinterest.java

--- CLEAN.CODE/src/main/java/EPAM/CLEAN/CODE/Interest/simpleinterest.java
+++ CLEAN.CODE/src/main/java/EPAM/CLEAN/CODE/Interest/simpleinterest.java
@@ -1,8 +1,6 @@
 package EPAM.CLEAN.CODE.Interest;
-import EPAM.CLEAN.CODE.printer.*;
  public class simpleinterest
  {
-Printer p=new Printer();
 public void calculatesimpleinterest(int p,int t,double r)
 	{
 	double sinterest=(p*t*r)/100;
